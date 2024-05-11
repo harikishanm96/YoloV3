d6cf45374ba79d8b195ef9bb2f6a24cfd946dbcf
harikishanm96 <harikishanm96@gmail.com>
2024-05-12 03:14:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ def test(cfg,
          weights=None,
          batch_size=16,
          img_size=416,
-         conf_thres=0.001,
+         conf_thres=0.1,
          iou_thres=0.6,  # for nms
          save_json=False,
          single_cls=False,
@@ -100,6 +100,7 @@ def test(cfg,
 
             # Run NMS
             t = torch_utils.time_synchronized()
+            # print("inf_out: ", inf_out)
             output = non_max_suppression(inf_out, conf_thres=conf_thres, iou_thres=iou_thres)  # nms
             t1 += torch_utils.time_synchronized() - t
 
@@ -200,10 +201,11 @@ def test(cfg,
 
 
 if __name__ == '__main__':
+    
     parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--cfg', type=str, default='cfg/yolov3-spp.cfg', help='*.cfg path')
-    parser.add_argument('--data', type=str, default='data/coco2014.data', help='*.data path')
-    parser.add_argument('--weights', type=str, default='weights/yolov3-spp-ultralytics.pt', help='weights path')
+    parser.add_argument('--cfg', type=str, default='./cfg/yolov3-custom.cfg', help='*.cfg path')
+    parser.add_argument('--data', type=str, default='./data/customdata/custom.data', help='*.data path')
+    parser.add_argument('--weights', type=str, default='./weights/yolov3-spp-ultralytics.pt', help='weights path')
     parser.add_argument('--batch-size', type=int, default=16, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=416, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
